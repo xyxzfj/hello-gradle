6f33e8c01e2f9df9cb680bacfd1dce4722cba046
xyxzfj <xyxzfj@gmail.com>
2016-05-26 17:51:24 +0800

    init project

---
+++ app/src/androidTest/java/viky/study/app/hellogradle/ApplicationTest.java
@@ -0,0 +1,13 @@
+package viky.study.app.hellogradle;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
